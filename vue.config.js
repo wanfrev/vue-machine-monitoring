63248d943aeff14bc5c7d969e57719c608a147fd
LuisCrespoURU <crespoluish2003@gmail.com>
2025-12-12 14:24:31 -0400

    iconos

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  pwa: {
+    name: "MachineHub",
+    themeColor: "#ffffff",
+    msTileColor: "#ffffff",
+    iconPaths: {
+      // Usamos los iconos generados a partir de K11BOX
+      favicon32: "img/icons/pwa-64x64.png",
+      favicon16: "img/icons/pwa-64x64.png",
+      appleTouchIcon: "img/icons/apple-touch-icon-180x180.png",
+      maskIcon: "img/icons/maskable-icon-512x512.png",
+      msTileImage: "img/icons/mstile-150x150.png",
+    },
+  },
 });
